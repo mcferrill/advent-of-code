196087dd518fa1ca034d28184bb2396ab534ba92
Micah Ferrill <mcferrill@gmail.com>
2023-12-04 19:58:14 -0600

    Finished day 4 part 2.

--- 2023/day4.go
+++ 2023/day4.go
@@ -7,35 +7,41 @@ import (
 	"strings"
 )
 
-
 func main() {
   data, _ := os.ReadFile("day4.txt")
   lines := strings.Split(string(data), "\n")
 
   r, _ := regexp.Compile(`\d+`)
 
-  sum := 0
-  for _, line := range lines {
+  copies := make(map[int]int)
+  for i, line := range lines {
     if (len(line) < 1) {
       continue
     }
+
     parts := strings.Split(strings.Split(line, ": ")[1], "|")
     winningNumbers := r.FindAllString(parts[0], -1)
     myNumbers := r.FindAllString(parts[1], -1)
+
+    copies[i] += 1
     total := 0
     for _, myNumber := range myNumbers {
       for _, winningNumber := range winningNumbers {
         if (myNumber == winningNumber) {
-          if (total == 0) {
-            total = 1
-          } else {
-            total *= 2
-          }
+          total += 1
         }
       }
     }
-    sum += total
+    for j := 0; j < total; j++ {
+      copies[i+j+1] += copies[i]
+    }
+    // fmt.Println(i, total, copies)
+    // return
   }
 
+  sum := 0
+  for _, v := range copies {
+    sum += v
+  }
   fmt.Println(sum)
 }
