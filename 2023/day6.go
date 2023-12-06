cd701f8c2cc064b53749af1dff90bd90a8022e4c
Micah Ferrill <mcferrill@gmail.com>
2023-12-06 07:11:01 -0600

    Finished day 6 part 2.

--- 2023/day6.go
+++ 2023/day6.go
@@ -9,33 +9,40 @@ import (
 	"strings"
 )
 
+func winConditions(time int, distance int) int {
+  start := float64(distance) / float64(time)
+  if (start != math.Trunc(start)){
+    start = math.Trunc(start) + 1
+  }
+
+  // Validate start
+  for i := int(start); i <= time-i; i++ {
+    if (i * (time-i) > distance) {
+      start = float64(i)
+      break
+    }
+  }
+
+  // Validate end
+  var end int
+  for i := time-1; i >= int(start); i-- {
+    if (i * (time-i) > distance) {
+      end = i
+      break
+    }
+  }
+
+  return end + 1 - int(start)
+}
+
 func main() {
   data, _ := os.ReadFile("day6.txt")
   r, _ := regexp.Compile(`\d+`)
   lines := strings.Split(string(data), "\n")
-  time := r.FindAllString(lines[0], -1)
-  distance := r.FindAllString(lines[1], -1)
-  total := 0
-  for i, race := range time {
-    t, _ := strconv.Atoi(race)
-    d, _ := strconv.Atoi(distance[i])
-    start := float64(d) / float64(t)
-    if (start != math.Trunc(start)){
-      start = math.Trunc(start) + 1
-    }
-
-    var winCondition []int
-    for i := int(start); i <= t-1; i++ {
-      if (i * (t-i) > d) {
-        winCondition = append(winCondition, i)
-      }
-    }
-    if (total == 0) {
-      total = len(winCondition)
-    } else {
-      total *= len(winCondition)
-    }
-  }
-  fmt.Println(total)
+  time := strings.Join(r.FindAllString(lines[0], -1), "")
+  distance := strings.Join(r.FindAllString(lines[1], -1), "")
+  t, _ := strconv.Atoi(time)
+  d, _ := strconv.Atoi(distance)
+  fmt.Println(winConditions(t, d))
 }
 
