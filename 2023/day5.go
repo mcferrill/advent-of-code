8c1eee9e1e2eeca8c6d0acb109129ab0088a71c5
Micah Ferrill <mcferrill@gmail.com>
2023-12-06 19:54:13 -0600

    WIP day 5 part 2

--- 2023/day5.go
+++ 2023/day5.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
 	"os"
 	"slices"
@@ -8,27 +9,35 @@ import (
 	"strings"
 )
 
-func find(start int, end int, sourceMap [][]int) int {
+func find(source int, sourceMap [][]int) int {
   for _, row := range sourceMap {
-    dStart := row[0]
-    // dEnd := row[1]
-    sStart := row[2]
-    sEnd := row[3]
+    dStart := row[2]
+    // dEnd := row[3]
+    sStart := row[0]
+    sEnd := row[1]
 
-    if (start > sEnd || end < sStart) {
+    if (source > sEnd || source < sStart) {
       continue
     }
 
-    offset := max(start, sStart) - sStart
-    return dStart + offset
-
+    return dStart + source - sStart
   }
-  return 0
+  return source
+}
+
+func validSeed(number int, seeds [][]int) bool {
+  for _, seedRange := range seeds {
+    if (number >= seedRange[0] && number <= seedRange[1]) {
+      return true
+    }
+  }
+  return false
 }
 
 func main() {
   data, _ := os.ReadFile("day5.txt")
   sections := strings.Split(string(data), "\n\n")
+
   var maps [][][]int
   for _, section := range sections[1:] {
     var sectionMap [][]int
@@ -46,24 +55,31 @@ func main() {
     maps = append(maps, sectionMap)
   }
 
-  seeds := strings.Split(sections[0], " ")[1:]
-
-  var results []int
-  for i := 0; i <= len(seeds) - 1; i += 2 {
-    start, _ := strconv.Atoi(seeds[i])
-    length, _ := strconv.Atoi(seeds[i+1])
-
-    number := find(start, start + length, maps[0])
-    for _, section := range maps[1:] {
-      number = find(number, number, section)
-      if (number == 0) {
-        break
-      }
-    }
-    results = append(results, number)
-
+  var seeds [][]int
+  numbers := strings.Split(sections[0], " ")[1:]
+  for i := 0; i <= len(numbers) - 1; i += 2 {
+    start, _ := strconv.Atoi(numbers[i])
+    length, _ := strconv.Atoi(numbers[i + 1])
+    seeds = append(seeds, []int{start, start + length})
   }
 
-  fmt.Println(slices.Min(results))
+  locations := maps[len(maps)-1]
+  slices.SortFunc(locations, func(a, b []int) int {
+    return cmp.Compare(a[0], b[0])
+  })
+
+  for _, locationRange := range locations {
+    for number := locationRange[0]; number <= locationRange[1]; number++ {
+      traversalNumber := number
+      for i := len(maps) -1; i > -1; i-- {
+        traversalNumber = find(traversalNumber, maps[i])
+      }
+      valid := validSeed(number ,seeds)
+      if (valid) {
+        fmt.Println(number)
+        return
+      }
+    }
+  }
 }
 
