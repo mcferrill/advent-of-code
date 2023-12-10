3794f7229998580bb069b3a302eef4f43bd5f221
Micah Ferrill <mcferrill@gmail.com>
2023-12-09 20:44:56 -0600

    Finished day 8 part 2.

--- 2023/day8.go
+++ 2023/day8.go
@@ -6,10 +6,69 @@ import (
 	"strings"
 )
 
+var nodeMap map[string][]string
+var directions []int
+
+func part1() {
+  currentNode := "AAA"
+  step := 0
+  for currentNode != "ZZZ" {
+    leftOrRight := directions[step % len(directions)]
+    currentNode = nodeMap[currentNode][leftOrRight]
+    step++
+  }
+
+  fmt.Println(step)
+}
+
+// Adapted from https://siongui.github.io/2017/06/03/go-find-lcm-by-gcd/
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+  for b != 0 {
+    t := b
+    b = a % b
+    a = t
+  }
+  return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+  result := a * b / GCD(a, b)
+
+  for i := 0; i < len(integers); i++ {
+    result = LCM(result, integers[i])
+  }
+
+  return result
+}
+
+func part2() {
+  // Make array of nodes ending with "A"
+  var nodes []string
+  for k := range nodeMap {
+    if k[2] == 'A' {
+      nodes = append(nodes, k)
+    }
+  }
+
+  var steps []int
+  for _, node := range nodes {
+    step := 0
+    for node[2] != 'Z' {
+      leftOrRight := directions[step % len(directions)]
+      node = nodeMap[node][leftOrRight]
+      step++
+    }
+    steps = append(steps, step)
+  }
+  fmt.Println(LCM(steps[0], steps[1], steps[2:]...))
+}
+
 func main() {
   data, _ := os.ReadFile("day8.txt")
   lines := strings.Split(string(data), "\n")
-  var directions []int
+
   for _, step := range lines[0] {
     if step == 'R' {
       directions = append(directions, 1)
@@ -18,7 +77,7 @@ func main() {
     }
   }
 
-  nodeMap := make(map[string][]string)
+  nodeMap = make(map[string][]string)
   for _, line := range lines[2:] {
     if line == "" {
       continue
@@ -33,13 +92,5 @@ func main() {
     nodeMap[name] = []string{left, right}
   }
 
-  currentNode := "AAA"
-  step := 0
-  for currentNode != "ZZZ" {
-    leftOrRight := directions[step % len(directions)]
-    currentNode = nodeMap[currentNode][leftOrRight]
-    step++
-  }
-
-  fmt.Println(step)
+  part2()
 }
