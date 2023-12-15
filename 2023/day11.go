5c5aeef07c4a775863df7d784e56e3cc9a8d0f49
Micah Ferrill <mcferrill@gmail.com>
2023-12-15 07:06:41 -0600

    Finished day11.

--- 2023/day11.go
+++ 2023/day11.go
@@ -12,53 +12,50 @@ type Point struct {
   raw string
 }
 
-var grid [][]Point
+var columns [][]Point
 var galaxies []Point
+var multiplier = 1000000
 
 func main() {
-  data, _ := os.ReadFile("tmp.txt")
-  var lines []string
-  for _, line := range strings.Split(string(data), "\n") {
+  data, _ := os.ReadFile("day11.txt")
+  verticalOffset := 0
+  for y, line := range strings.Split(string(data), "\n") {
     if line == "" {
       continue
     }
     tmp := strings.TrimSpace(strings.ReplaceAll(line, ".", " "))
-    lines = append(lines, line)
     if len(tmp) == 0 {
-      lines = append(lines, line)
-    }
-  }
-
-  grid = make([][]Point, len(lines[0]))
-  for y := 0; y <= len(lines)-1; y++ {
-    line := strings.TrimSpace(lines[y])
-    for x, char := range line {
-      if len(grid[x]) == 0 {
-        grid[x] = make([]Point, len(lines))
+      verticalOffset += multiplier-1
+    } else {
+      for x, char := range line {
+        p := Point{x, y + verticalOffset, string(char)}
+        if len(columns) - 1 < x {
+          columns = append(columns, []Point{p})
+        } else {
+          columns[x] = append(columns[x], p)
+        }
       }
-      p := Point{x, y, string(char)}
-      grid[x][y] = p
     }
   }
 
-  for x := 0; x < len(grid); x++ {
-    col := grid[x]
-    emptyCol := true
-    for _, point := range col {
+  horizontalOffset := 0
+  for _, column := range columns {
+    empty := true
+    for _, point := range column {
       if point.raw == "#" {
-        point.x = x
-        emptyCol = false
+        point.x += horizontalOffset
         galaxies = append(galaxies, point)
+        empty = false
       }
     }
-    if emptyCol {
-      grid = append(grid[:x+1], grid[x:]...)
-      x++
+    if empty {
+      horizontalOffset += multiplier-1
     }
   }
 
   sum := 0
   for i, galaxy := range galaxies {
+    fmt.Println(galaxy)
     for j, other := range galaxies {
       if j <= i {
         continue
