c6b04ef4675c6cdab577b408f9c8924933cd0d83
Micah Ferrill <mcferrill@gmail.com>
2023-12-20 08:14:33 -0600

    Finished day 13 part 2.

--- 2023/day13.go
+++ 2023/day13.go
@@ -7,7 +7,8 @@ import (
 	"strings"
 )
 
-func mirrorOffset(lines []string) int {
+func mirrorOffsets(lines []string) []int {
+  var offsets []int
   for i := range lines {
     length := min(i, len(lines) - i)
     if length < 1 {
@@ -18,17 +19,19 @@ func mirrorOffset(lines []string) int {
     copy(right, lines[i:i+length])
     slices.Reverse(right)
     if strings.Join(left, "") == strings.Join(right, "") {
-      return i
+      offsets = append(offsets, i)
     }
   }
-  return 0
+  return offsets
 }
 
-func valueForGroup(group string) int {
+func valuesForGroup(group string) []int {
   lines := strings.Split(strings.TrimSpace(group), "\n")
-  result := mirrorOffset(lines)
-  if result > 0 {
-    return result * 100
+  var offsets []int
+  for _, result := range mirrorOffsets(lines) {
+    if result > 0 {
+      offsets = append(offsets, result * 100)
+    }
   }
 
   columns := make([]string, len(lines[0]))
@@ -37,16 +40,40 @@ func valueForGroup(group string) int {
       columns[j] += string(char)
     }
   }
-  return mirrorOffset(columns)
+  for _, result := range mirrorOffsets(columns) {
+    if result > 0 {
+      offsets = append(offsets, result)
+    }
+  }
+
+  return offsets
+}
+
+func recalculate(group string, i int, char string) []int {
+  sep := "."
+  if char == "." {
+    sep = "#"
+  }
+  s := strings.Join([]string{group[:i], group[i+1:]}, sep)
+  return valuesForGroup(s)
 }
 
 func main() {
   data, _ := os.ReadFile("day13.txt")
   sum := 0
   for i, group := range strings.Split(string(data), "\n\n") {
-    result := valueForGroup(group)
-    sum += result
-    if result == 0 {
+    results := valuesForGroup(group)
+    for j, char := range group {
+      if strings.Contains(".#", string(char)) {
+        for _, tmp := range recalculate(group, j, string(char)){
+          if tmp != 0 && !slices.Contains(results, tmp) {
+            results = append(results, tmp)
+          }
+        }
+      }
+    }
+    sum += results[1]
+    if len(results) == 0 {
       fmt.Println("No match for group", i)
     }
   }
