bd2c21f3deefefe80e13297cfe2a3824e3bd5f89
Micah Ferrill <mcferrill@pm.me>
2024-12-03 06:59:54 -0600

    Day 3.2

--- 2024/day03.ts
+++ 2024/day03.ts
@@ -1,18 +1,29 @@
 
 import fs from 'node:fs';
 
-function part1(instructions: string[]): number {
+function part1(instructions: RegExpMatchArray[]): number {
 	let sum = 0
 	for (const inst of instructions) {
-		const left = parseInt(inst[1])
-		const right = parseInt(inst[2])
-		sum += (left * right)
+		sum += (parseInt(inst[1]) * parseInt(inst[2]))
+	}
+	return sum
+}
+
+function part2(instructions: RegExpMatchArray[]): number {
+	let sum = 0
+	let enabled = true
+	for (const inst of instructions) {
+		if (inst[0] == 'do()') enabled = true
+		else if (inst[0] == "don't()") enabled = false
+		else if (enabled) sum += (parseInt(inst[2]) * parseInt(inst[3]))
 	}
 	return sum
 }
 
 fs.readFile('day03.txt', (_, data) => {
-	const instructions = [...data.toString().matchAll(/mul\((\d+)\,(\d+)\)/g)]
+	const muls = [...data.toString().matchAll(/mul\((\d+)\,(\d+)\)/g)]
+	const mulsAndConditions = [...data.toString().matchAll(/(mul\((\d+)\,(\d+)\))|don\'t\(\)|do\(\)/g)]
 
-	console.log(part1(instructions))
+	console.log(part1(muls))
+	console.log(part2(mulsAndConditions))
 })
